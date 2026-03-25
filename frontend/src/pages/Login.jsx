ef8bccca998394b4550ab5eddf785cbdd840696d
chiragarya67 <chiragarya5678@gmail.com>
2026-03-25 12:13:41 +0530

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 import { serverUrl } from '../main'
 import { useDispatch, useSelector } from 'react-redux'
 import { setSelectedUser, setuserData } from '../redux/userSlice'
-import User from '../../../backend/models/userModel'
+
 
 const Login = () => {
    let navigate = useNavigate() 
