2a590d9e1a0890968b79191fba67febbecb3dc6f
chirag <chiragarya5678@gmail.com>
2026-03-25 11:55:58 +0530

    Change server URL to production endpoint

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from 'react-redux'
 import { store } from './redux/store.js'
 
-export const serverUrl = "http://localhost:3000"
+export const serverUrl = "https://yochat-lwyu.onrender.com"
 
 
 createRoot(document.getElementById('root')).render(
